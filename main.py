329688bd5bbb6c13b34c95e4eecaf58161fdaa43
Michael Skelly <skellystudios@gmail.com>
2016-02-04 22:09:34 +0000

    Init

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from subprocess import call
+
+app = Flask(__name__)
+
+@app.route('/')
+def update_jekyll():
+	try:
+		skelly_io_dir = "/var/www/skelly-io"
+		call(["git", "pull"], cwd=skelly_io_dir)
+		call(["jekyll", "build"], cwd=skelly_io_dir)
+	except:
+		pass
+	return 'ok'
+
+if __name__ == '__main__':
+	app.run()
